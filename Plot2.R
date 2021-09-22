8056729a0c8752994e8fe5906a54ee38dd42d8ac
Sachin Sharma <udgamsachin@gmail.com>
2021-09-22 19:47:28 +0530

    Create Plot2.R

---
+++ Plot2.R
@@ -0,0 +1,62 @@
+
+
+library(tidyr)
+library(tidyverse)
+library(lubridate)
+library(dplyr)
+
+household <- read.table("household_power_consumption.txt", sep = ";", header = TRUE)
+
+
+view(household)
+
+#Read 2890 rows that contain information on 2007-02-01 and 2007-02-02
+
+household_new <-read.table("household_power_consumption.txt", header=TRUE,sep=";", skip=66636, 
+                           nrows=2890, col.names=names(household), na.strings=c("?"),
+                           colClasses=c("character", "character","numeric","numeric","numeric","numeric",
+                                        "numeric","numeric","numeric"))
+
+
+
+
+
+
+# Filtering out the final rows which are  required : 
+
+household_new <- household_new[1:2880,] 
+
+
+view(household_new)
+
+#Converting Date and Time variables to Date/Time format
+
+household_new$Date<-as.Date(household_new$Date, format = "%d/%m/%Y")
+household_new$Time<-strptime(household_new$Time,format = "%H:%M:%S")
+
+
+
+
+
+
+
+#Plot 2. Global active power consumption over time
+par(mfrow=c(1,1),mar=c(5,4.5,4,2))
+plot(household_new$Time,household_new$Global_active_power, ylab="Global Active Power (kilowatts)", 
+     xlab="", pch =".", type="l")
+
+
+
+# Saving the plot2 in png format 
+
+png("plot2.png", width=480, height=480)
+par(mfrow=c(1,1),mar=c(5,4.5,4,2))
+plot(household_new$Time,household_new$Global_active_power, ylab="Global Active Power (kilowatts)", 
+     xlab="", pch =".", type="l")
+
+
+dev.off()
+
+
+
+
